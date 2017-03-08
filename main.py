080df9d81165748016b543e8a74318b34a5f66ff
miniwa <blarch3030@gmail.com>
2017-03-08 21:05:39 +0100

    Added logging

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 """
 Main script.
 """
+import logging
+from logging.handlers import RotatingFileHandler
 import smtplib
 from email.mime.text import MIMEText
 from api import get_direct, get_all
@@ -10,13 +12,27 @@ from settings_local import CONF
 
 
 def main():
+    # Setup log.
+    logger = logging.getLogger("bostad")
+    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+    handler = RotatingFileHandler("bostad.log",
+                maxBytes=1024*100, backupCount=3)
+    handler.setFormatter(formatter)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+    # Setup storage
     storage = Storage.create_or_open("housing.db")
+
+    logger.info("--- START ---")
     result = get_direct()
 
+    logger.info("Retrieved {0} housings".format(len(result)))
     if result:
         body = ""
         for housing in result:
             if not storage.has_address(housing.address):
+                logger.info("Address {0} not previously seen".format(housing.address))
                 body += housing.address
                 body += "\nHyra: {0}kr".format(housing.rent)
                 body += "\nURL: {0}\n\n".format(housing.url)
@@ -33,13 +49,16 @@ def main():
         msg["To"] = CONF["TO"]
 
         try:
+            logger.info("Connecting to SMTP server")
             smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
             smtp_server.starttls()
             smtp_server.login(CONF["AUTH_USER"], CONF["AUTH_PWD"])
             smtp_server.send_message(msg)
             smtp_server.quit()
+            logger.info("Message sent")
         except Exception as exc:
-            print(exc)
+            logger.error("Unhandled exception: {0}".format(exc))
+    logger.info("--- END ---")
 
 
 if __name__ == '__main__':
